18c2af5f9b1cb12f9bc200a2c1ab0c26a94568d0
Mark Mandriota <mandriotamark@gmail.com>
2023-10-08 11:23:51 +0200

    add which key mode

--- init.el
+++ init.el
@@ -2,3 +2,17 @@
  (expand-file-name
   "README.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key whole-line-or-region rustic lsp-ui god-mode go-mode
+			   go-autocomplete doom-themes company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
