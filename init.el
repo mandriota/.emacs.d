031f37b337e9ebed22f8f7cec819d55bbd577ec2
Mark Mandriota <mandriotamark@gmail.com>
2023-10-08 23:34:14 +0200

    update to straight; typst mode; tree sitter sources

--- init.el
+++ init.el
@@ -8,8 +8,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key whole-line-or-region rustic lsp-ui god-mode go-mode
-			   go-autocomplete doom-themes company)))
+   '(typst-ts-mode which-key whole-line-or-region rustic lsp-ui god-mode
+				   go-mode go-autocomplete doom-themes company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
