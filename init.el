9bce9ee8c80b10d8a2cf0f5c92d89b0e97ab703b
Mark Mandriota <mandriotamark@gmail.com>
2023-10-22 12:48:40 +0200

    Add magit support & hotkeys to risizing windows

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-safe-remote-resources
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
    '(typst-ts-mode which-key whole-line-or-region rustic lsp-ui god-mode
 				   go-mode go-autocomplete doom-themes company)))
