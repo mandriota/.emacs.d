c6899251498e4968647d5d851d32287d5a0bbcbb
Mark Mandriota <mandriotamark@gmail.com>
2023-11-30 18:56:59 +0100

    use rounded corners

--- early-init.el
+++ early-init.el
@@ -1,2 +1,6 @@
-(setq package-enable-at-startup nil)
+;; appereance
+(add-to-list 'default-frame-alist '(undecorated-round . t))
 (load-theme 'wombat)
+
+;; other
+(setq package-enable-at-startup nil)
