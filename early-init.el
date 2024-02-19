ee440d3e8229f866988054b621ff13d0d738f03d
Mark Mandriota <mandriotamark@gmail.com>
2024-02-19 21:29:12 +0100

    add yasnippet; ob-go; lsp optimizations

--- early-init.el
+++ early-init.el
@@ -2,5 +2,9 @@
 (add-to-list 'default-frame-alist '(undecorated-round . t))
 (load-theme 'wombat)
 
+;; packages build options
+(setenv "LSP_USE_PLISTS" "true")
+
 ;; other
 (setq package-enable-at-startup nil)
+
--- init.el
+++ init.el
@@ -7,11 +7,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "4e2e42e9306813763e2e62f115da71b485458a36e8b4c24e17a2168c45c9cf9d" default))
  '(org-safe-remote-resources
    '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
-   '(typst-ts-mode which-key whole-line-or-region rustic lsp-ui god-mode
-				   go-mode go-autocomplete doom-themes company)))
+   '(typst-ts-mode which-key whole-line-or-region rustic lsp-ui god-mode go-mode go-autocomplete doom-themes company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
